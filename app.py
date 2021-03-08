bc458e6aa01b6b4e870f4da066d6b2cd1ad5eada
Krzysztof Kopera <krzysztof.kopera@insc.pl>
2021-03-08 08:15:19 +0000

    Redis store page open counter

--- app.py
+++ app.py
@@ -1,20 +1,44 @@
 from flask import Flask, render_template
 from prometheus_flask_exporter import PrometheusMetrics
-
+import redis, time
 import socket
 
-try: 
-    server_name = socket.gethostname() 
-    server_ip = socket.gethostbyname(server_name) 
-except: 
-    server_name = "Unable to get Hostname"
-    server_ip = "Unable to get IP"
+
+def get_set_hit_count(set= True):
+    retries = 5
+    while True:
+        try:
+            if set:
+                return cache.incr('hits')
+            else:
+                return cache.get('hits')
+        except redis.exceptions.ConnectionError as exc:
+            if retries == 0:
+                raise exc
+            retries -= 1
+            time.sleep(0.5)
+
+def get_host_information():
+    try: 
+        server_name = socket.gethostname() 
+        server_ip = socket.gethostbyname(server_name) 
+    except: 
+        server_name = "Unable to get Hostname"
+        server_ip = "Unable to get IP"
+    
+    return server_ip, server_name
 
 
 app = Flask(__name__)
 metrics = PrometheusMetrics(app)
+cache = redis.Redis(host='redis', port=6379)
 
 @app.route("/")
 def index():
-    return render_template('index.html', server_ip = server_ip, server_name = server_name)
+    server_info = get_host_information()
+    count = get_set_hit_count()
+    return render_template('index.html', server_ip = server_info[0], server_name = server_info[1], count = count)
     
+@app.route("/counter")
+def counter():
+    return get_set_hit_count(set= False)
